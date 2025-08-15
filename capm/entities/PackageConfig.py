ff5d9b45a9852707eba9175fc912248c18e313e5
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2025-08-15 17:27:10 +0200

    perf: ⚡️ Optimize build and run

--- capm/entities/PackageConfig.py
+++ capm/entities/PackageConfig.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 @dataclass
 class PackageConfig:
     id: str
-    image: str | None = None
     args: str | None = None
     extra_args: str | None = None
     workspace_mode: str | None = None
